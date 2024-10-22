4b54f1e29d78c1677acb6c72df64420dc89242e3
aybun <73546349+aybun@users.noreply.github.com>
2024-10-22 22:12:00 +0700

    add p3

--- cpu-api/p3.zig
+++ cpu-api/p3.zig
@@ -3,22 +3,28 @@ const print = std.debug.print;
 const getpid = std.os.linux.getpid;
 const fork = std.os.linux.fork;
 const exit = std.process.exit;
-//p2.zig
 const sleep = std.time.sleep;
 const waitpid = std.os.linux.waitpid;
 
-// This example shows how to wait for the child process to finish.
-// Next, How do I exit with an arbitrary flag?
 pub fn main() !void {
-    std.debug.print("hello world (pid:{d})\n", .{getpid()});
+    print("hello world (pid:{d})\n", .{getpid()});
     const rc = fork();
 
     if (rc < 0) {
         print("fork failed\n", .{});
         exit(1);
     } else if (rc == 0) {
+        // Child process
+        // done via LLM
         print("hello, I am child (pid:{d})\n", .{getpid()});
-        sleep(1_000_000);
+
+        const command = [_][]const u8{ "wc", "p3.zig" };
+        const result = try std.process.Child.run(.{
+            .allocator = std.heap.page_allocator,
+            .argv = &command,
+        });
+
+        print("{s}\n", .{result.stdout});
     } else {
 
         //https://ratfactor.com/zig/forking-is-cool
