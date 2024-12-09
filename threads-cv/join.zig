5c1dbbbcab0be2bfff5dcab7ab6bc7aac352e1a1
aybun <73546349+aybun@users.noreply.github.com>
2024-12-09 11:43:29 +0700

    add join.zig

---
+++ threads-cv/join.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const print = std.debug.print;
+
+var m = std.Thread.Mutex{};
+var c = std.Thread.Condition{};
+var done: i64 = 0;
+
+fn child() !void {
+    print("child\n", .{});
+    std.time.sleep(1_000_000);
+    m.lock();
+    done = 1;
+    c.signal();
+    m.unlock();
+}
+
+pub fn main() !void {
+    print("parent: begin\n", .{});
+    _ = try std.Thread.spawn(.{}, child, .{});
+    m.lock();
+
+    while (done == 0) {
+        c.wait(&m);
+    }
+
+    m.unlock();
+    print("parent: end\n", .{});
+}
