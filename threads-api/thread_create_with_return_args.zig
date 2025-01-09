4824346f1207cbb0c66c636098a510716758d314
aybun <73546349+aybun@users.noreply.github.com>
2025-01-09 11:19:30 +0700

    Update thread_create_with_return_args.zig

--- threads-api/thread_create_with_return_args.zig
+++ threads-api/thread_create_with_return_args.zig
@@ -10,11 +10,11 @@ const myret_t = struct {
     y: i64,
 };
 
-fn worker(args: *myarg_t, rets: []myret_t) !void {
+fn worker(args: *myarg_t, rets: *myret_t) !void {
     std.debug.print("args {d} {d}\n", .{ args.a, args.b });
 
-    rets[0].x = 1;
-    rets[0].y = 2;
+    rets.*.x = 1;
+    rets.*.y = 2;
 }
 
 pub fn main() !void {
@@ -27,12 +27,12 @@ pub fn main() !void {
         .b = 20,
     };
 
-    const rvals: []myret_t = try allocator.alloc(myret_t, 1);
+    const rvals = try allocator.create(myret_t);
+    defer allocator.destroy(rvals);
 
     const thread_1 = try std.Thread.spawn(.{}, worker, .{ &args, rvals });
 
     thread_1.join();
-    defer allocator.free(rvals);
 
-    std.debug.print("returned {d} {d}\n", .{ rvals[0].x, rvals[0].y });
+    std.debug.print("returned {d} {d}\n", .{ rvals.*.x, rvals.*.y });
 }
